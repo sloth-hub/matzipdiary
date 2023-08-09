03d1bb8112cd9fbaefbc78073a7eabc064edb872
sloth-hub <madamhh@gmail.com>
2023-08-09 21:07:20 +0900

    init: 프로젝트 초기 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='container'>
+            <div>
+                <input type='text' className='input-text' placeholder='내용을 입력하세요' />
+            </div>
+        </div>
+    );
 }
 
 export default App;
