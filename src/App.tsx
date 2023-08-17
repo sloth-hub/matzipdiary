ae11eca46ba0537ec70bd184a7bae33576db6722
sloth-hub <madamhh@gmail.com>
2023-08-17 19:44:24 +0900

    Feat: 로그인한 유저 정보 가져오는 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import React from "react";
-import {AppRouter} from "./Routes/Router"
+import React, { useEffect, useState } from "react";
+import { AppRouter } from "./Routes/Router"
+import { UserInterface } from "./interfaces/user.interface";
+import { onAuthStateChanged } from "firebase/auth";
+import { db, firebaseAuth } from "./Firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { Login } from "./pages/Login";
 
 const App = () => {
+
+    const [init, setInit] = useState<boolean>(false);
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [userObj, setUserObj] = useState<UserInterface | null>(null);
+
+    useEffect(() => {
+        onAuthStateChanged(firebaseAuth, async (user) => {
+            if (user) {
+                const q = query(
+                    collection(db, "users"),
+                    where("uid", "==", user.uid)
+                );
+                const querySnapshot = await getDocs(q);
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data();
+                    setUserObj({
+                        uid: data.uid,
+                        email: data.email,
+                        nickname: data.nickname,
+                        date_created: data.date_created,
+                    });
+                });
+                setIsLoggedIn(true);
+            } else {
+                setUserObj(null);
+            }
+            setInit(true);
+        });
+    });
+
+
     return (
         <div className="container">
-            <AppRouter />
+            {init ?
+                <AppRouter
+                    isLoggedIn={isLoggedIn} userObj={userObj} />
+                : <div className="loader">Loading...</div>
+            }
         </div>
     );
 }
--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -3,12 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SignUp } from "../pages/SignUp";
 import { Login } from "../pages/Login"
 import { Home } from "../pages/Home";
+import { UserInterface } from "../interfaces/user.interface";
 
-export const AppRouter = () => {
+type RouterType = {
+    isLoggedIn: boolean,
+    userObj: UserInterface | null
+}
+
+export const AppRouter = ({ isLoggedIn, userObj }: RouterType) => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Home userObj={userObj} />} />
                 <Route path="/signup/*" element={<SignUp />} />
                 <Route path="/login/*" element={<Login />} />
             </Routes>
