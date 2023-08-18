2340dfd551bedbac44f805a5481c0cd5346076ae
sloth-hub <madamhh@gmail.com>
2023-08-18 21:21:38 +0900

    Chore: 로그아웃시 catch 코드 추가 및 유저 이름 표시 코드 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,18 @@ export const Home = ({ userObj }: any) => {
 
 
     const logout = () => {
-        firebaseAuth.signOut();
-        window.location.reload();
+        firebaseAuth.signOut()
+            .then(() => {
+                window.location.reload();
+            }).catch(err => {
+                console.log(`${err.code} - ${err.message}`);
+            });
     }
 
     return (
         <>
             <h1>home page</h1>
-            <p>어서오세요 {userObj.nickname}님!</p>
+            <p>{userObj ? `Hello, ${userObj.nickname}!` : "goodbye!"}</p>
             <button onClick={logout}>로그아웃</button>
         </>
     )
