058d3173cabf0317fc9aa075b30969a418a860c7
sloth-hub <madamhh@gmail.com>
2023-08-21 23:23:18 +0900

    Feat: 홈 화면에 유저가 작성한 글 불러오기 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { firebaseAuth } from "../Firebase";
+import { db, firebaseAuth } from "../Firebase";
+import { Notes } from "../Components/Notes";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export const Home = ({ userObj }: any) => {
 
+    const [notes, setNotes] = useState([{}]);
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+    const getNotes = async () => {
+        const q = query(
+            collection(db, "notes"),
+            where("uid", "==", userObj.uid));
+        const querySnapshot = await getDocs(q);
+        const data = querySnapshot.docs.map(doc => ({
+            ...doc.data()
+        }));
+        setNotes(data);
+    }
 
     const logout = () => {
         firebaseAuth.signOut()
@@ -13,11 +31,18 @@ export const Home = ({ userObj }: any) => {
             });
     }
 
+
+
     return (
         <>
             <h1>home page</h1>
             <p>{userObj ? `Hello, ${userObj.nickname}!` : "goodbye!"}</p>
             <button onClick={logout}>로그아웃</button>
+            <div className="notes">
+                {notes.map((note, index) =>
+                    <Notes key={index} note={note} />
+                )}
+            </div>
         </>
     )
 }
