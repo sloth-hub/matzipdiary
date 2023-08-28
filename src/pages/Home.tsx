35a773bc3f986377a99cc278ecec7b7707b82473
sloth-hub <madamhh@gmail.com>
2023-08-28 19:56:49 +0900

    Style: 일기가 없는 경우 home 화면 설정 및 일기쓰기 버튼 css 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { Note } from "../Components/Note";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { NoteInterface } from "../interfaces/note.interface";
 import { Link } from "react-router-dom";
+import { PiWarningFill } from "react-icons/pi";
 
 export const Home = ({ userObj }: any) => {
 
@@ -27,14 +28,33 @@ export const Home = ({ userObj }: any) => {
         setNotes(data);
     }
 
+    const changeStyle = (e: React.MouseEvent) => {
+        const target = e.target;
+        (target as HTMLElement).innerText = "일기쓰기";
+        (target as HTMLElement).classList.add("active");
+    }
+
+    const removeStyle = (e:React.MouseEvent) => {
+        const target = e.target;
+        (target as HTMLElement).innerText = "+";
+        (target as HTMLElement).classList.remove("active");
+    }
+
     return (
         <>
             <div className="notes">
-                {notes.map((note, index) =>
-                    <Note key={index} note={note} />
-                )}
+                {notes.length > 0 ?
+                    notes.map((note, index) =>
+                        <Note key={index} note={note} />
+                    )
+                    :
+                    <div className="warn">
+                        <PiWarningFill size={"2em"} />
+                        <p>일기가 없습니다.<br /> 우측 하단의 버튼을 눌러 일기를 써보세요!</p>
+                    </div>
+                }
             </div>
-            <Link to="/write" className="write-btn">+</Link>
+            <Link to="/write" className="write-btn" onMouseOver={changeStyle} onMouseOut={removeStyle}>+</Link>
         </>
     )
 }
