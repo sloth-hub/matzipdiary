d1666fb6b0acf8191731ca3281def0f4273a89b2
sloth-hub <madamhh@gmail.com>
2023-09-11 20:42:31 +0900

    Feat: 디테일 페이지 카카오맵 지도 추가

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,23 +1,51 @@
-import React from "react";
-import { useLocation, useNavigate } from "react-router";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate, } from "react-router";
 
 export const Detail = () => {
 
-    const location = useLocation();
+    useEffect(() => {
+        mapInit();
+    }, []);
+
+    const statedata = useLocation();
     const navigate = useNavigate();
     const { date_created,
         date_visited,
         foodCategory,
         placeName,
         images,
-        text } = location.state;
+        location,
+        text } = statedata.state;
+
+    const mapInit = () => {
+
+        const kakao = (window as any).kakao;
+        const mapContainer = document.getElementById("map");
+        const mpaOption = {
+            center: new kakao.maps.LatLng(location.lat, location.lng), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        }
+        const map = new kakao.maps.Map(mapContainer, mpaOption);
+        const markerPosition = new kakao.maps.LatLng(location.lat, location.lng);
+        const marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+        marker.setMap(map);
+        const iwContent = `<div>
+        <span>${placeName}</span>
+        <a href="https://map.kakao.com/link/map/${placeName},${location.lat},${location.lng}" target="_blank">큰지도보기</a>
+        <a href="https://map.kakao.com/link/to/${placeName},${location.lat},${location.lng}">길찾기</a>
+        </div>`;
+        const iwPosition = new kakao.maps.LatLng(location.lat, location.lng);
+        const infowindow = new kakao.maps.InfoWindow({
+            position : iwPosition, 
+            content : iwContent 
+        });
+        infowindow.open(map, marker); 
+    }
 
     return (
         <div className="detail">
-            <div>
-                {images ? images.map((image: any, i: number) => <img src={image.fileUrl} key={i} />)
-                    : <></>}
-            </div>
             <div>
                 <span>작성일</span>
                 <span>{date_created}</span>
@@ -33,6 +61,11 @@ export const Detail = () => {
             <div>
                 <span>가게명</span>
                 <span>{placeName}</span>
+            </div>
+            <div id="map"></div>
+            <div>
+                {images ? images.map((image: any, i: number) => <img src={image.fileUrl} key={i} />)
+                    : <></>}
             </div>
             <p>{text}</p>
             <button type="button" onClick={() => navigate("/")}>뒤로</button>
