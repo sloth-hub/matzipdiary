9974608853e0f1c074cc8fc5e4560e146c68c89b
sloth-hub <madamhh@gmail.com>
2023-09-12 18:10:23 +0900

    Style: 메인 note 레이아웃 및 css 수정

--- src/Components/Note.tsx
+++ src/Components/Note.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { BiChevronRight } from "react-icons/bi";
+import { BiChevronRight, BiCalendar } from "react-icons/bi";
 import { HiLocationMarker } from "react-icons/hi";
 
 export const Note = ({ note }: any) => {
@@ -17,28 +17,26 @@ export const Note = ({ note }: any) => {
         <div className="note">
             <div className="img-wrap">
                 {images ? images.length > 1 ?
-                    <div className="img-box" key={images[0].fileUrl.slice(-12)}><img src={images[0].fileUrl} loading="lazy" /><span className="imgcount">+{images.length-1}</span></div>
-                    : <div className="img-box only" key={images[0].fileUrl.slice(-12)}><img src={images[0].fileUrl} loading="lazy" /></div>
+                    <><img src={images[0].fileUrl} key={images[0].fileUrl.slice(-12)} loading="lazy" /><span className="imgcount">+{images.length - 1}</span></>
+                    : <><img src={images[0].fileUrl} key={images[0].fileUrl.slice(-12)} loading="lazy" /></>
                     : <></>}
             </div>
             <div className="content-wrap">
-                <div>
+                <div className="date">
+                    <BiCalendar/>
                     <span>{date_visited}</span>
-                    <span>{foodCategory}</span>
                 </div>
                 <div className="place">
                     <HiLocationMarker />
+                    <span className="category">{foodCategory}</span>
                     <span>{placeName}</span>
                 </div>
                 <p>{text}</p>
             </div>
-            <div className="bottom-wrap">
-                <span className="date">{date_created.slice(0, 10)}</span>
-                <Link to={`/note/${id}`} state={note} className="more">
-                    <span>more</span>
-                    <BiChevronRight size={"1.2em"} />
-                </Link>
-            </div>
+            <Link to={`/note/${id}`} state={note} className="more">
+                <span>more</span>
+                <BiChevronRight size={"1.2em"} />
+            </Link>
         </div>
     )
 }
