4c1f4c90a54c2b62fed358d4be448c9850c7b0c0
sloth-hub <madamhh@gmail.com>
2023-09-13 19:16:38 +0900

    Chore: 카카오맵 대신 주소만 표시하도록 수정

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate, } from "react-router";
 import { BiChevronLeft } from "react-icons/bi";
 
 export const Detail = () => {
 
-    useEffect(() => {
-        mapInit();
-    }, []);
-
+    const [address, setAddress] = useState({ lotAddr: "", roadAddr: "" });
     const statedata = useLocation();
     const navigate = useNavigate();
     const { date_created,
@@ -18,60 +15,44 @@ export const Detail = () => {
         location,
         text } = statedata.state;
 
-    const mapInit = () => {
+    useEffect(() => {
+        mapInit();
+    }, []);
 
+    const mapInit = async () => {
         const kakao = (window as any).kakao;
-        const mapContainer = document.getElementById("map");
-        const mpaOption = {
-            center: new kakao.maps.LatLng(location.lat, location.lng), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
-        }
-        const map = new kakao.maps.Map(mapContainer, mpaOption);
-        const markerPosition = new kakao.maps.LatLng(location.lat, location.lng);
-        const marker = new kakao.maps.Marker({
-            position: markerPosition
+        const geocoder = new kakao.maps.services.Geocoder();
+        await geocoder.coord2Address(location.lng, location.lat, (snapshot: any) => {
+            setAddress({
+                lotAddr: snapshot[0].address.address_name,
+                roadAddr: snapshot[0].road_address.address_name,
+            });
         });
-        marker.setMap(map);
-        const iwContent = `<div class="info">
-        <span>${placeName}</span>
-        <div>
-        <a href="https://map.kakao.com/link/map/${placeName},${location.lat},${location.lng}" target="_blank">큰지도보기</a>
-        <a href="https://map.kakao.com/link/to/${placeName},${location.lat},${location.lng}">길찾기</a>
-        </div>
-        </div>`;
-        const iwPosition = new kakao.maps.LatLng(location.lat, location.lng);
-        const infowindow = new kakao.maps.InfoWindow({
-            position: iwPosition,
-            content: iwContent
-        });
-        infowindow.open(map, marker);
-    }
-
-    const toggleMap = () => {
-        const map = document.getElementById("map");
-        map!.classList.toggle("active");
     }
 
     return (
         <div className="detail">
-            <div>
-                <span className="title">작성일</span>
-                <span>{date_created}</span>
+            <div className="date">
+                <div>
+                    <span className="title">작성일</span>
+                    <span>{date_created}</span>
+                </div>
+                <div>
+                    <span className="title">방문일자</span>
+                    <span>{date_visited}</span>
+                </div>
             </div>
-            <div>
-                <span className="title">방문일자</span>
-                <span>{date_visited}</span>
+            <div className="place">
+                <div className="placename">
+                    <span className="title">{placeName}</span>
+                    <span>{foodCategory}</span>
+                </div>
+                <div className="address">
+                    <span>{address && address.roadAddr}</span>
+                    <span>{address && address.lotAddr}</span>
+                    <a href={`https://map.kakao.com/link/map/${placeName},${location.lat},${location.lng}`} target="_blank">큰지도보기</a>
+                </div>
             </div>
-            <div>
-                <span className="title">카테고리</span>
-                <span>{foodCategory}</span>
-            </div>
-            <div>
-                <span className="title">가게명</span>
-                <span>{placeName}</span>
-            </div>
-            <button type="button" onClick={toggleMap}>지도 보기</button>
-            <div id="map" className="map"></div>
             <div className="img-wrap">
                 {images ? images.map((image: any, i: number) => <img src={image.fileUrl} key={i} />)
                     : <></>}
