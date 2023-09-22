126d38b9d7e662d94ae6416dfe193bc3ec3e2d6a
sloth-hub <madamhh@gmail.com>
2023-09-22 18:36:31 +0900

    Chore: 노트폼 에디터 툴바 옵션(글자크기, 글자색, 배경색) 변경

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -8,10 +8,26 @@ export const Editor = ({ onChange }: Props) => {
 
     const modules = {
         toolbar: [
-            [{ 'header': [1, 2, false] }],
+            [{ 'header': [1, 2, 3, false] }],
             ['bold', 'italic', 'underline', 'strike', 'link'],
             [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [{ 'align': [] }, { 'color': [] }, { 'background': [] }]
+            [{ 'align': [] }, {
+                'color': [
+                    "#181818", "#331C5C", "#1C385C", "#1C5C44", "#81741C", "#9B400B", "#921C11",
+                    "#515152", "#563096", "#305C96", "#309671", "#B8A72B", "#EB720A", "#DC2918",
+                    "#838386", "#7E5CB8", "#5C84B8", "#5CB896", "#D0C258", "#F5943D", "#E65C4C",
+                    "#cdcbcb", "#A48DCC", "#8DA8CC", "#8DCCB5", "#DED58B", "#F8B882", "#EE948B",
+                    "#ffffff", "#C9BCE0", "#BCCDE0", "#BCE0D2", "#EBE6BB", "#FBD1B2", "#F5BCB8"
+                ]
+            }, {
+                'background': [
+                    "#181818", "#331C5C", "#1C385C", "#1C5C44", "#81741C", "#9B400B", "#921C11",
+                    "#515152", "#563096", "#305C96", "#309671", "#B8A72B", "#EB720A", "#DC2918",
+                    "#838386", "#7E5CB8", "#5C84B8", "#5CB896", "#D0C258", "#F5943D", "#E65C4C",
+                    "#cdcbcb", "#A48DCC", "#8DA8CC", "#8DCCB5", "#DED58B", "#F8B882", "#EE948B",
+                    "#ffffff", "#C9BCE0", "#BCCDE0", "#BCE0D2", "#EBE6BB", "#FBD1B2", "#F5BCB8"
+                ]
+            }]
         ]
     }
 
